ed0b63b52bc8ca85490ce58191e01e9795eab253
flynnmlp <flynnmlp@gmail.com>
2023-02-18 17:12:50 +0100

    move flake8 configuration from __init__ to pyproject.toml

--- src/pylomena/__init__.py
+++ src/pylomena/__init__.py
@@ -1,5 +1,4 @@
-
-from .types import *  # noqa: F401,F403
-from .matcher import *  # noqa: F401,F403
-from .api import *  # noqa: F401,F403
+from .types import *
+from .matcher import *
+from .api import *
 
