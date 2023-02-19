c9ecee48887ba5f87a718c9bbfee63e709728d7c
flynnmlp <flynnmlp@gmail.com>
2023-02-19 16:27:36 +0100

    bump version numbers and reorganize tests to include main runner

---
+++ tests/test_main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import pathlib
+import subprocess
+import sys
+
+
+def main():
+    this = pathlib.Path(__file__).name
+    
+    tests = [
+        str(path)
+        for path
+        in pathlib.Path("tests").glob("*.py")
+        if path.name != this
+    ]
+    
+    if not tests:
+        raise ValueError("No tests found!")
+    
+    sys.exit(subprocess.call((sys.executable, "-m", "unittest", *tests)))
+
+
+if __name__ == "__main__":
+    main()
+
